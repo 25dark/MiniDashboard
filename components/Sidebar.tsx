9c8d6b0116f73f14201601b52c41e3165111e89c
25Dark <henrydanso6@gmail.com>
2024-04-01 18:44:47 +0000

    :Sidebar && UserItem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@ import UserItem from "./UserItem";
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col w-[300px] min-w-[300px] border-r bg-slate-400 min-h-screen">
+    <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
       <div>
         <UserItem />
       </div>
--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -1,16 +1,18 @@
 "use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function UserItem() {
   return (
-    <div>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-
-      <p>Henry Yaw Danso</p>
-      <p>example@email.com</p>
+    <div className="flex items-center justify-between gap-2 border rounded-[20px] p-4">
+      <div
+        className="avatar rounded-full min-h-12 min-w-12  text-white text-font-700  
+        bg-emerald-500 flex items-center justify-center"
+      >
+        HYD
+      </div>
+      <div className="grow">
+        <p className="text-[16px] font-bold">Henry Yaw Danso</p>
+        <p className="text-[12px] text-neutral-500">example@email.com</p>
+      </div>
     </div>
   );
 }
